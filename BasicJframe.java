56c7d10729de734b94817101ce70860d62ab6c47
pontshomaila <pontshomaila246@gmail.com>
2025-10-30 00:33:28 +0200

    Add files via upload

---
+++ BasicJframe.java
@@ -0,0 +1,25 @@
+
+package basicjframe;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+
+/**
+ *
+ * @author pontshomaila
+ */
+public class BasicJframe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //i need to declare my variables
+        MyFirstFrame myFrame;
+        
+        myFrame=new MyFirstFrame();
+        
+    }
+    
+}
